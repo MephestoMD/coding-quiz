361f82f403e8e59775bfa191363434064843db5c
Maxson Green <maxson.green@gmail.com>
2021-02-17 22:44:00 -0500

    Created objects to handle questions

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 // Select timer element by ID
 let timer = document.getElementById("timer");
 
-// Define variable for remaining time
-let timeLeft = 5;
+// Select start button by ID
+let startButton = document.getElementById("start-button");
 
+// Define variable for remaining time
+let timeLeft = 75;
+
+// Create an array of objects to handle each question and its corrects
+let questionBank = [
+    {
+        question: "Commonly used data types do NOT include: ___",
+        options: ["a) strings", "b) booleans", "c) alerts", "d) numbers"],
+        correct: "c) alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within: ___",
+        options: ["a) quotations", "b) curly braces", "c) parentheses", "d) square brackets"],
+        correct: "c) parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store: ___",
+        options: ["a) numbers", "b) other arrays", "c) booleans", "d) all of the above"],
+        correct: "d) all of the above"
+    },
+    {
+        question: "String value must be enclosed within ___ when being assigned to variables.",
+        options: ["a) commas", "b) curly braces", "c) quotations", "d) parentheses"],
+        correct: "c) quotations"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ___",
+        options: ["a) JavaScript", "b) terminal/bash", "c) alerts", "d) console.log"],
+        correct: "d) console.log"
+    },
+]
 
 function startTimer () {
     // Create interval function to handle the timer
@@ -11,6 +42,7 @@ function startTimer () {
     // Initial time display to handle starting time remaining value
     timer.textContent = "Time: " + timeLeft;  
 
+    // Call setInterval function to create timer countdown
     let timeId = setInterval(function () {
 
         timeLeft --;
@@ -23,6 +55,12 @@ function startTimer () {
         }
 
     }, 1000);
+    questionPopulate()
+
+}
+
+function questionPopulate() {
+
 }
 
 // function gameOver {
@@ -30,4 +68,4 @@ function startTimer () {
 // }
 
 
-startTimer()
+startButton.addEventListener("click", startTimer);
